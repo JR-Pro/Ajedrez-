229426d18a6780ddbfcd4c57d670f19a488bcfca
JR-Pro <jeremyrodas123333@gmail.com>
2022-05-23 17:51:26 -0600

    Tablero

--- Tablero.py
+++ Tablero.py
@@ -1,8 +1,31 @@
-import pygame, sys 
-
+import pygame
+negro = (0,0,0)
+blanco = (255,255,255)
 pygame.init()
-pygame.display.set_mode(resolution = 500, 500)
-pygame.display.flip()
-pygame.event.get()
 
+dimen = [600,600]
+screen = pygame.display.set_mode(dimen)
+pygame.display.set_caption("TABLERO")
+
+running = True
+ancho = int(dimen[0] / 8)
+alto = int(dimen[1] / 8)
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(blanco)
+    color = 0
+    for i in range(0, dimen[0],ancho):
+         for j in range(0, dimen[1],alto):
+             if color % 2 == 0:
+                pygame.draw.rect(screen, negro, [i, j, ancho, alto], 0)
+             else:
+                pygame.draw.rect(screen, blanco, [i, j, ancho, alto], 0)
+             color += 1
+         color += 1
+
+    pygame.display.flip()
+
+pygame.quit()
 
