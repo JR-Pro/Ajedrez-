00b2f44cb9eb6dfed0f4142d6a46460ae5eebe87
JR-Pro <jeremyrodas123333@gmail.com>
2022-05-26 16:07:15 -0600

    s3

--- Tablero.py
+++ Tablero.py
@@ -1,33 +1,35 @@
 import pygame
+class ajadrez():
+    def tablero():
+        negro = (0,0,0)
+        blanco = (255,255,255)
+        pygame.init()
 
-negro = (0,0,0)
-blanco = (255,255,255)
-pygame.init()
+        dimen = [600,600]
+        screen = pygame.display.set_mode(dimen)
+        pygame.display.set_caption("Ajedrez")
 
-dimen = [600,600]
-screen = pygame.display.set_mode(dimen)
-pygame.display.set_caption("Ajedrez")
+        running = True
+        ancho = int(dimen[0] / 8)
+        alto = int(dimen[1] / 8)
+        reloj = pygame.time.Clock()
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            screen.fill(blanco)
+            color = 0
+            for i in range(0, dimen[0],ancho):
+                for j in range(0, dimen[1],alto):
+                    if color % 2 == 0:
+                        pygame.draw.rect(screen, negro, [i, j, ancho, alto], 0)
+                    else:
+                        pygame.draw.rect(screen, blanco, [i, j, ancho, alto], 0)
+                    color += 1
+                color += 1
 
-running = True
-ancho = int(dimen[0] / 8)
-alto = int(dimen[1] / 8)
-reloj = pygame.time.Clock()
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    screen.fill(blanco)
-    color = 0
-    for i in range(0, dimen[0],ancho):
-         for j in range(0, dimen[1],alto):
-             if color % 2 == 0:
-                pygame.draw.rect(screen, negro, [i, j, ancho, alto], 0)
-             else:
-                pygame.draw.rect(screen, blanco, [i, j, ancho, alto], 0)
-             color += 1
-         color += 1
+            pygame.display.flip()
+            reloj.tick(5)
+        pygame.quit()
 
-    pygame.display.flip()
-    reloj.tick(5)
-pygame.quit()
-
+ajadrez.tablero()
